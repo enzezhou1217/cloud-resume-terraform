48e63d8bd32bdb3e94f432112cf5f05c7e6507d3
Enze Zhou <ZOO@EnzedeMacBook-Pro.local>
2023-02-05 19:48:32 +0800

    modified lambda handler

--- main.tf
+++ main.tf
@@ -102,7 +102,7 @@ resource "aws_lambda_function" "cloud-resume-lambda-function" {
   filename         = "cloud-resume-lambda-function.zip"
   function_name    = "cloud-resume-lambda-function"
   role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "index.lambda_handler"
+  handler          = "cloud-resume-lambda-function.lambda_handler"
   runtime          = "python3.8"
 }
 resource "aws_lambda_function_url" "for-api-gateway" {
